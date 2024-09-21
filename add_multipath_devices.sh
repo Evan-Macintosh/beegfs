6e2cb8eca91dc8138772b335994b0db87377eedf
Evan-Macintosh <thedoctorisinnow@gmail.com>
2024-09-21 17:08:00 -0400

    Update add_multipath_devices.sh
    add modprobe

--- add_multipath_devices.sh
+++ add_multipath_devices.sh
@@ -2,6 +2,7 @@
 # change 838.4G to whatever size you see from lsblk
 systemctl stop multipathd.service
 for HDD in $(lsblk | grep 838.4G | awk '{printf "%s ",$1}'); do multipath -a /dev/$HDD; done
+modprobe dm_multipath	# to make sure it's really there
 systemctl start multipathd.service
 echo ""
 multipath -ll
