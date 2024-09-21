a601b6bc35edf49d5e93e319c0a273467633d728
Evan-Macintosh <thedoctorisinnow@gmail.com>
2024-09-21 19:56:37 -0400

    Update create_beegfs_filesystem.sh
    remove -s options because i dont know how to effectively use them and they're scary

--- create_beegfs_filesystem.sh
+++ create_beegfs_filesystem.sh
@@ -4,6 +4,6 @@
 ##########
 BEEGFS_MASTER=lcs-s1-hsn0
 beegfs-setup-mgmtd -p /data/beegfs/
-beegfs-setup-meta -p /mnt/beegfs-meta -s 100 -m $BEEGFS_MASTER
-beegfs-setup-storage -p /mnt/beegfs-data0 -s 200 -m $BEEGFS_MASTER
-beegfs-setup-storage -p /mnt/beegfs-data1 -s 201 -m $BEEGFS_MASTER
+beegfs-setup-meta -p /mnt/beegfs-meta -m $BEEGFS_MASTER
+beegfs-setup-storage -p /mnt/beegfs-data0 -m $BEEGFS_MASTER
+beegfs-setup-storage -p /mnt/beegfs-data1 -m $BEEGFS_MASTER
