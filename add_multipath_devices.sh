130fe6dfd7a17f2f50f118d20d44921aabac1d33
Evan-Macintosh <thedoctorisinnow@gmail.com>
2024-11-23 15:32:02 -0500

    Update add_multipath_devices.sh
    do multipath for SSDs in ProLiant DL380p G8 (despite them not being truly multipath'd, smh my head)

--- add_multipath_devices.sh
+++ add_multipath_devices.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # change 838.4G to whatever size you see from lsblk
 systemctl stop multipathd.service
-for HDD in $(lsblk | grep 838.4G | awk '{printf "%s ",$1}'); do multipath -a /dev/$HDD; done
+for HDD in $(lsblk | grep 838.4G | awk '{printf "%s ",$1}'); do multipath -a /dev/$HDD; done  # 900GB SAS HDDs
+for HDD in $(lsblk | grep 186.3G | awk '{printf "%s ",$1}'); do multipath -a /dev/$HDD; done  # 200GB SAS SSDs
 modprobe dm_multipath	# to make sure it's really there
 systemctl start multipathd.service
-echo ""
 multipath -ll
