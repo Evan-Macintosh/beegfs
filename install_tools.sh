39b2884b96fa8ed2247d2ba238656520474b8865
Evan-Macintosh <thedoctorisinnow@gmail.com>
2025-02-23 11:10:02 -0500

    add sed append contrib to sources.list

--- install_tools.sh
+++ install_tools.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-apt install multipath-tools device-mapper zfsutils-linux xfsprogs
+# https://www.cyberciti.biz/faq/installing-zfs-on-debian-12-bookworm-linux-apt-get/
+sed -r -i'.BAK' 's/^deb(.*)$/deb\1 contrib/g' /etc/apt/sources.list
+apt update
+apt install multipath-tools device-mapper zfsutils-linux xfsprogs zfs-zed
 systemctl stop multipathd.service
